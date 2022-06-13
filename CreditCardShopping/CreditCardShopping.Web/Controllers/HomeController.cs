bd339665b2455373ce89d8e89d0aaa6818600435
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-06-13 09:54:12 -0300

    primeiro commit

---
+++ CreditCardShopping/CreditCardShopping.Web/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CreditCardShopping.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CreditCardShopping.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var products = new List<ProductViewModel>();
+            var product1 = new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Notebook Acer Nitro 5",
+                Price = 5199.99,
+                Description = "GTX 1650, intel core i5, 8GB de RAM, 512GB SSD",
+                ImageURL = "https://www.kabum.com.br/conteudo/descricao/113442/img/header-image.png"
+            };
+
+            products.Add(product1);
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public JsonResult GetProductById(int id)
+        {
+            var products = new List<ProductViewModel>();
+            var product1 = new ProductViewModel()
+            {
+                Id = 1,
+                CategoryName = "Notebook Gamer",
+                Name = "Notebook Acer Nitro 5",
+                Price = 5199.99,
+                Description = "GTX 1650, intel core i5, 8GB de RAM, 512GB SSD",
+                ImageURL = "https://www.kabum.com.br/conteudo/descricao/113442/img/header-image.png"
+            };
+
+            products.Add(product1);
+
+            foreach(var product in products)
+            {
+                if(product.Id == id)
+                {
+                    return Json(product1);
+                }
+            }
+
+            return Json("Produto não encontrado");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CreditCardShopping/CreditCardShopping.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CreditCardShopping.Web.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageURL { get; set; }
+        public int Count { get; set; }
+    }
+}
