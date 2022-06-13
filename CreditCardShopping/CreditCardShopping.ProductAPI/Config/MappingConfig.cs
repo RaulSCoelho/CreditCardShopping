5da0ba0264baec3d29f97b61edaff8bb57c58aaa
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-06-13 14:43:15 -0300

    automapper e repository

---
+++ CreditCardShopping/CreditCardShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CreditCardShopping.ProductAPI.Data.ValueObjects;
+using CreditCardShopping.ProductAPI.Model;
+
+namespace CreditCardShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig =  new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ CreditCardShopping/CreditCardShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CreditCardShopping.ProductAPI.Data.ValueObjects;
+
+namespace CreditCardShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<List<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> Delete(long id);
+    }
+}
